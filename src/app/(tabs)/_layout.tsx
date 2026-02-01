d853a7edce84329877bc35c8bdb2696abffb9474
David Adokuru <45779326+adokuru@users.noreply.github.com>
2026-02-01 21:36:40 +0100

    updates

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,27 +1,40 @@
+import { Colors } from "@/constants/theme";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
+import { DynamicColorIOS } from "react-native";
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
+    <NativeTabs
+      labelStyle={{
+        color: DynamicColorIOS({
+          dark: Colors.dark.text,
+          light: Colors.light.text,
+        }),
+      }}
+      tintColor={DynamicColorIOS({
+        dark: Colors.dark.tint,
+        light: Colors.light.tint,
+      })}
+    >
       <NativeTabs.Trigger name="index">
-        <Icon sf="square.grid.2x2" />
+        <Icon sf="square.grid.2x2" drawable="" />
         <Label>Dashboard</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="(courses)">
-        <Icon sf="book.closed" />
+        <Icon sf="book.closed" drawable="" />
         <Label>Courses</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="calendar">
-        <Icon sf="calendar" />
+        <Icon sf="calendar" drawable="" />
         <Label>Classes</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="chat">
-        <Icon sf="message" />
+        <Icon sf="message" drawable="" />
         <Label>Chat</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.circle" />
+        <Icon sf="person.circle" drawable="" />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
